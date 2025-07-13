5622e6b86a36162191aa28204443169eb0c77724
hbenpitsu <hbenpitsu15@gmail.com>
2025-07-14 06:55:45 +0900

    改名

--- HAll.hs
+++ HAll.hs
@@ -31,14 +31,14 @@ f = TM {
                 writeAtHead (ENCODED_TM TM {
                     externalTapes = 1,
                     initInnerTapes = [TAPE 0 []],
-                    transition = \_ [_, wd] -> case lastCall of
+                    transition = \_ [ipt, word] -> case lastCall of
                         NO_CALL_JUST_BEFORE -> CALL_TR machine [1]
                         CALL_JUST_BEFORE ACCEPT -> HALT_TR ACCEPT
                         CALL_JUST_BEFORE REJECT -> HALT_TR REJECT
                 })]
             ENCODED_TM TM {
                 externalTapes = externalTapes',
-                initInnerTapes = [TAPE 0 wd'],
+                initInnerTapes = [TAPE 0 word'],
                 transition = transition'
             } -> case word of
                 BLANK -> HALT_TR ACCEPT
@@ -48,7 +48,7 @@ f = TM {
                     writeAtHead (ENCODED_TM TM {
                         externalTapes = externalTapes',
                         -- accumulate the word on the tape
-                        initInnerTapes = [TAPE 0 (wd' ++ [word])],
+                        initInnerTapes = [TAPE 0 (word' ++ [word])],
                         transition = transition'
                     })]
 
